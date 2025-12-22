5c32ccd95fd5f35980f769798b234db716067ae4
rRakarare <rRene333@googlemail.com>
2025-12-22 13:06:58 +0100

    i18n working

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,7 @@
 import { createIsomorphicFn } from "@tanstack/react-start";
 import { getCookie } from "@tanstack/react-start/server";
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import causualTranslation from "./dict/causual.json";
 import formalTranslation from "./dict/formal.json";
@@ -18,23 +19,25 @@ export const defaultNS = "translation";
 
 const i18nCookieName = "i18nextLng";
 
-i18n.use(initReactI18next).init({
-	resources,
-	defaultNS,
-	fallbackLng: "fo",
-	supportedLngs: ["fo", "ca"],
-	detection: {
-		order: ["cookie"],
-		lookupCookie: i18nCookieName,
-		caches: ["cookie"],
-		cookieMinutes: 60 * 24 * 365,
-	},
-	interpolation: { escapeValue: false },
-});
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources,
+		defaultNS,
+		fallbackLng: "fo",
+		supportedLngs: ["fo", "ca"],
+		detection: {
+			order: ["cookie"],
+			lookupCookie: i18nCookieName,
+			caches: ["cookie"],
+			cookieMinutes: 60 * 24 * 365,
+		},
+		interpolation: { escapeValue: false },
+	});
 
 export const setSSRLanguage = createIsomorphicFn().server(async () => {
 	const language = getCookie(i18nCookieName);
-	console.log("lang", language);
 	await i18n.changeLanguage(language || "fo");
 });
 
